6b3d41a5c53c9209d780a936b2aac44a0901428d
Beth Carretta <bethcarretta@gmail.com>
2023-03-23 16:37:22 -0400

    basic functionality

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,49 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    """display home page"""
+    html = """
+      <html>
+        <body>
+          <h1>A Simple Calculator built with Flask</h1>
+          <a href='/add'>Add</a>
+          <a href='/subtract'>Subtract</a>
+          <a href='/multiply'>Multiply</a>
+          <a href='/divide'>Divide</a>
+        </body>
+      </html>
+    """
+    return html
+
+@app.route('/add')
+def display_add():
+    print(request.args) # ImmutableMultiDict([('a', '1'), ('b', '2')])
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    sum = add(a,b)
+    return f"<p>The sum of {a} and {b} is: {sum}</p>"
+
+@app.route('/subtract')
+def display_subtract():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    remainder = sub(a, b)
+    return f"<p>The remainder of {a} minus {b} is: {remainder}</p>"
+
+@app.route('/multiply')
+def display_multiply():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    product = mult(a, b)
+    return f"<p>The product of {a} and {b} is: {product}</p>"
+
+@app.route('/divide')
+def display_divide():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    result = int(div(a, b))
+    return f"<p>{a} divided by {b} is: {result}</p>"
---
+++ greet/app.py
@@ -0,0 +1,55 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    """display the home page"""
+    html = """
+    <html>
+      <body>
+        <h1>Home Page</h1>
+        <p>This is my first Flask app</p>
+        <a href='/welcome'>Welcome Page</a>
+        <a href='/welcome/home'>Welcome Home Page</a>
+        <a href='/welcome/back'>Welcome Back Page</a>
+      </body>
+    </html>
+    """
+    return html
+
+@app.route('/welcome')
+def show_welcome():
+    """ display the welcome page"""
+    html = """
+    <html>
+      <body>
+        <h1>Welcome Page</h1>
+      </body>
+    </html>
+    """
+    return html
+
+@app.route('/welcome/home')
+def show_welcome_home():
+    """display welcome home page"""
+    html = """
+    <html>
+      <body>
+        <h1>Welcome Home Page</h1>
+      </body>
+    </html>
+    """
+    return html
+
+@app.route('/welcome/back')
+def show_welcome_back():
+    """display welcome back page"""
+    html = """
+    <html>
+      <body>
+        <h1>Welcome Back Page</h1>
+      </body>
+    </html>
+    """
+    return html
